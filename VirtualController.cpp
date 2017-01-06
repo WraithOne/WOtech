85eb925717fb2aef6895ff11864b4825f7f8e14b
WraithOne <WraithOne@Dark-Abyss.net>
2017-01-06 10:00:21 +0100

    Virtual_Controller -> Gamepad

--- VirtualController.cpp
+++ VirtualController.cpp
@@ -5,7 +5,7 @@
 ///			File: VirtualController.h
 ///
 ///			Created:	04.01.2017
-///			Edited:		04.01.2017
+///			Edited:		06.01.2017
 ///
 ////////////////////////////////////////////////////////////////////////////
 
@@ -28,13 +28,13 @@ namespace WOtech
 		{
 			switch (m_currentInput)
 			{
-			case WOtech::Gameframework::Current_Input::Gamepad:
+			case WOtech::Gameframework::Current_Input_Device::Gamepad:
 				UpdateGamepad();
 				break;
-			case WOtech::Gameframework::Current_Input::TouchandPen:
-				UpdateTouchandPen;
+			case WOtech::Gameframework::Current_Input_Device::TouchandPen:
+				UpdateTouchandPen();
 				break;
-			case WOtech::Gameframework::Current_Input::KeyboardandMouse:
+			case WOtech::Gameframework::Current_Input_Device::KeyboardandMouse:
 				UpdateKeyboardandMouse();
 				break;
 			default:
@@ -42,17 +42,17 @@ namespace WOtech
 			}
 		}
 
-		void VirtualController::setCurrentInput(Current_Input current)
+		void VirtualController::setCurrentInput(Current_Input_Device current)
 		{
 			m_currentInput = current;
 		}
 
-		Current_Input VirtualController::getCurrent()
+		Current_Input_Device VirtualController::getCurrent()
 		{
 			return m_currentInput;
 		}
 
-		Virtual_Gamepad VirtualController::getState()
+		Virtual_Controller VirtualController::getState()
 		{
 			return m_state;
 		}
@@ -60,40 +60,61 @@ namespace WOtech
 		{
 			m_currentGamepad = number;
 		}
-		void VirtualController::bindKeyboardKey(Virtual_Gamepad_Buttons target, Windows::System::VirtualKey key)
+		void VirtualController::bindKeyboardKey(Virtual_Controller_Buttons target, Windows::System::VirtualKey key)
 		{
-			throw ref new Platform::NotImplementedException();
+			std::map<Virtual_Controller_Buttons, Windows::System::VirtualKey>::iterator it;
+			it = m_keyboardbinding.find(target);
+
+			if (it != m_keyboardbinding.end())
+			{
+				m_keyboardbinding[target] = key;
+			}
+			else
+			{
+				m_keyboardbinding.emplace(target, key);
+			}
 		}
 		void VirtualController::UpdateGamepad()
 		{
 			WOtech::Gamepad_State state = m_inputManager->GamepadState(m_currentGamepad);
-			m_state.Buttons.A = state.Buttons.A;
-			m_state.Buttons.B = state.Buttons.B;
-			m_state.Buttons.X = state.Buttons.X;
-			m_state.Buttons.Y = state.Buttons.Y;
-			m_state.Buttons.Menu = state.Buttons.Menu;
-			m_state.Buttons.View = state.Buttons.View;
-			m_state.Buttons.LeftShoulder = state.Buttons.LeftShoulder;
-			m_state.Buttons.LeftStick = state.Buttons.LeftStick;
-			m_state.Buttons.RightShoulder = state.Buttons.RightShoulder;
-			m_state.Buttons.RightStick = state.Buttons.RightStick;
+			m_state.Button_A = state.Buttons.A;
+			m_state.Button_B = state.Buttons.B;
+			m_state.Button_X = state.Buttons.X;
+			m_state.Button_Y = state.Buttons.Y;
+			m_state.Button_Menu = state.Buttons.Menu;
+			m_state.Button_View = state.Buttons.View;
+			m_state.Button_LeftShoulder = state.Buttons.LeftShoulder;
+			m_state.Button_LeftStick = state.Buttons.LeftStick;
+			m_state.Button_RightShoulder = state.Buttons.RightShoulder;
+			m_state.Button_RightStick = state.Buttons.RightStick;
 
-			m_state.Buttons.DPad.Up = state.DPad.Up;
-			m_state.Buttons.DPad.Down = state.DPad.Down;
-			m_state.Buttons.DPad.Left = state.DPad.Left;
-			m_state.Buttons.DPad.Right = state.DPad.Right;
+			m_state.DPad_Up = state.DPad.Up;
+			m_state.DPad_Down = state.DPad.Down;
+			m_state.DPad_Left = state.DPad.Left;
+			m_state.DPad_Right = state.DPad.Right;
 
-			m_state.Triggers.Left = state.triggers.Left;
-			m_state.Triggers.Right = state.triggers.Right;
+			m_state.Trigger_Left = state.triggers.Left;
+			m_state.Trigger_Right = state.triggers.Right;
 
-			m_state.Tumbsticks.LeftX = state.tumbsticks.LeftX;
-			m_state.Tumbsticks.LeftY = state.tumbsticks.LeftY;
-			m_state.Tumbsticks.RightX = state.tumbsticks.RightX;
-			m_state.Tumbsticks.RightY = state.tumbsticks.RightY;
+			m_state.Tumbstick_LeftX = state.tumbsticks.LeftX;
+			m_state.Tumbstick_LeftY = state.tumbsticks.LeftY;
+			m_state.Tumbstick_RightX = state.tumbsticks.RightX;
+			m_state.Tumbstick_RightY = state.tumbsticks.RightY;
 		}
 		void VirtualController::UpdateKeyboardandMouse()
 		{
-			throw ref new Platform::NotImplementedException();
+			for(std::map<Virtual_Controller_Buttons, Windows::System::VirtualKey>::iterator it = m_keyboardbinding.begin(); it != m_keyboardbinding.end(); ++it)
+			{
+				if (it->first == Virtual_Controller_Buttons::A)
+					m_state.Button_A = m_inputManager->KeyDown(it->second);
+				if (it->first == Virtual_Controller_Buttons::B)
+					m_state.Button_B = m_inputManager->KeyDown(it->second);
+				if (it->first == Virtual_Controller_Buttons::X)
+					m_state.Button_X = m_inputManager->KeyDown(it->second);
+				if (it->first == Virtual_Controller_Buttons::Y)
+					m_state.Button_Y = m_inputManager->KeyDown(it->second);
+
+			}
 		}
 		void VirtualController::UpdateTouchandPen()
 		{
--- VirtualController.h
+++ VirtualController.h
@@ -5,7 +5,7 @@
 ///			File: VirtualController.h
 ///
 ///			Created:	04.01.2017
-///			Edited:		04.01.2017
+///			Edited:		06.01.2017
 ///
 ////////////////////////////////////////////////////////////////////////////
 #ifndef WO_VIRTUALCONTROLLER_H
@@ -22,52 +22,55 @@ namespace WOtech
 {
 	namespace Gameframework
 	{
-		public enum class Current_Input
+		public enum class Current_Input_Device
 		{
 			Gamepad = 0,
 			TouchandPen,
 			KeyboardandMouse
 		};
+		public enum class Virtual_Controller_Buttons
+		{
+			A = 0,
+			B,
+			X,
+			Y,
+			LeftStick,
+			RightStick,
+			LeftShoulder,
+			RightShoulder,
+			View,
+			Menu,
+			DPad_Up,
+			DPad_Down,
+			DPad_Left,
+			DPad_Right
+		};
 
-		public value struct Virtual_Gamepad_DPad
+		public value struct Virtual_Controller
 		{
-			Platform::Boolean Up;
-			Platform::Boolean Down;
-			Platform::Boolean Left;
-			Platform::Boolean Right;
-		};
-		public value struct Virtual_Gamepad_Buttons
-		{
-			Virtual_Gamepad_DPad DPad;
-			Platform::Boolean A;
-			Platform::Boolean B;
-			Platform::Boolean X;
-			Platform::Boolean Y;
-			Platform::Boolean LeftStick;
-			Platform::Boolean RightStick;
-			Platform::Boolean LeftShoulder;
-			Platform::Boolean RightShoulder;
-			Platform::Boolean View;
-			Platform::Boolean Menu;
-		};
-		
-		public value struct Virtual_Gamepad_Trigger_State
-		{
-			float64 Left;
-			float64 Right;
-		};
-		public value struct Virtual_Gamepad_Tumbstick_State
-		{
-			float64 LeftX;
-			float64 LeftY;
-			float64 RightX;
-			float64 RightY;
-		};
-		public value struct Virtual_Gamepad
-		{
-			Virtual_Gamepad_Buttons Buttons;
-			Virtual_Gamepad_Trigger_State Triggers;
-			Virtual_Gamepad_Tumbstick_State Tumbsticks;
+			Platform::Boolean Button_A;
+			Platform::Boolean Button_B;
+			Platform::Boolean Button_X;
+			Platform::Boolean Button_Y;
+			Platform::Boolean Button_LeftStick;
+			Platform::Boolean Button_RightStick;
+			Platform::Boolean Button_LeftShoulder;
+			Platform::Boolean Button_RightShoulder;
+			Platform::Boolean Button_View;
+			Platform::Boolean Button_Menu;
+
+			Platform::Boolean DPad_Up;
+			Platform::Boolean DPad_Down;
+			Platform::Boolean DPad_Left;
+			Platform::Boolean DPad_Right;
+
+			float64 Trigger_Left;
+			float64 Trigger_Right;
+
+			float64 Tumbstick_LeftX;
+			float64 Tumbstick_LeftY;
+			float64 Tumbstick_RightX;
+			float64 Tumbstick_RightY;
 		};
 
 		public ref class VirtualController sealed
@@ -76,16 +79,16 @@ namespace WOtech
 			VirtualController(WOtech::InputManager^ input);
 			void Update();
 
-			void setCurrentInput(_In_ Current_Input current);
-			Current_Input getCurrent();
+			void setCurrentInput(_In_ Current_Input_Device current);
+			Current_Input_Device getCurrent();
 
-			Virtual_Gamepad getState();
+			Virtual_Controller getState();
 
 			// Gamepad
 			void bindGamepad(_In_ GamepadIndex number);
 
 			// Keyboard and Mice
-			void bindKeyboardKey(_In_ Virtual_Gamepad_Buttons target, _In_ Windows::System::VirtualKey key);
+			void bindKeyboardKey(_In_ Virtual_Controller_Buttons target, _In_ Windows::System::VirtualKey key);
 			// Touch and Pen
 
 		internal:
@@ -96,12 +99,14 @@ namespace WOtech
 			void UpdateTouchandPen();
 			
 		private:
-			WOtech::InputManager^	m_inputManager;
+			WOtech::InputManager^												m_inputManager;
 
-			Virtual_Gamepad			m_state;
+			Virtual_Controller													m_state;
 
-			Current_Input			m_currentInput;
-			WOtech::GamepadIndex	m_currentGamepad;
+			Current_Input_Device												m_currentInput;
+			WOtech::GamepadIndex												m_currentGamepad;
+			
+			std::map<Virtual_Controller_Buttons, Windows::System::VirtualKey>	m_keyboardbinding;
 		};
 	};
 };
