8f4205d71de6f0bf256190d4ce208f776711e897
WraithOne <WraithOne@WraithOne.de>
2018-02-17 15:02:20 +0100

    Fixed compiler error

--- Include/Utilities.h
+++ Include/Utilities.h
@@ -112,20 +112,20 @@ namespace WOtech
 		return radian;
 	}
 	// Converts Vector to Degree
-	inline float32 VectortoDegree(_In_ float32 x, _In_ float32 y)
-	{
-		float32 result = RadiantoDegree(VectortoRadian(y, x));
-
-		return result;
-	}
-	// Converts Vector to Degree
 	inline float32 VectortoRadian(_In_ float32 x, _In_ float32 y)
 	{
 		float32 result = atan2(y, x);
 
 		return result;
 	}
+	// Converts Vector to Degree
+	inline float32 VectortoDegree(_In_ float32 x, _In_ float32 y)
+	{
+		float32 result = RadiantoDegree(VectortoRadian(y, x));
 
+		return result;
+	}
+	
 	// Ceck if value is between min and max
 	template <typename T>
 	inline T InRange(_In_ T value, _In_ T min, _In_ T max)
